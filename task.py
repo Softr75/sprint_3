15048f715bc897e6671990a2ea54bbf672f1d9f8
Softr75 <menesoftic@gmail.com>
2025-11-20 00:45:32 +0600

    sdelal

--- task.py
+++ task.py
@@ -11,18 +11,15 @@ class OnlineSalesRegisterCollector:
     @property
     def name_items(self):
         return self.__name_items
-    
     @property
     def number_items(self):
         return self.__number_items
-    
+
     def add_item_to_cheque(self, name):
         if len(name) == 0 or len(name) > 40:
             raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
-        
         if name not in self.__item_price:
             raise NameError('Позиция отсутствует в товарном справочнике')        
-        
         else:
             self.__name_items.append(name)
             self.__number_items += 1
@@ -30,7 +27,6 @@ class OnlineSalesRegisterCollector:
     def delete_item_from_check(self, name):
         if name not in self.__name_items:
             raise NameError('Позиция отсутствует в чеке')
-        
         else:
             self.__name_items.remove(name)
             self.__number_items -= 1
@@ -53,34 +49,25 @@ class OnlineSalesRegisterCollector:
         for item in self.__name_items:
             if self.__tax_rate.get(item) == 20:
                 twenty_percent_tax.append(item)
-
         for item in twenty_percent_tax:
             total.append(self.__item_price[item])
-
         total_sum = sum(total)
-
         if self.__number_items > 10:
             total_sum = total_sum * 0.9
-
         tax = total_sum * 0.2
         return tax
     
     def ten_percent_tax_calculation(self):
         ten_percent_tax = []
         total = []
-
         for item in self.__name_items:
             if self.__tax_rate.get(item) == 10:
                 ten_percent_tax.append(item)
-
         for item in ten_percent_tax:
             total.append(self.__item_price[item])
-
         total_sum = sum(total)
-
         if self.__number_items > 10:
             total_sum = total_sum * 0.9
-
         tax = total_sum * 0.1
         return tax
 
@@ -89,11 +76,10 @@ class OnlineSalesRegisterCollector:
     
     @staticmethod
     def get_telephone_number(telephone_number):
-        if type(telephone_number).__name__ != int:
+        try:
+            _ = telephone_number + 0
+        except TypeError:
             raise ValueError('Необходимо ввести цифры')
-
-        elif telephone_number > 9999999999:
+        if telephone_number > 9999999999 or telephone_number < 1000000000:
             raise ValueError('Необходимо ввести 10 цифр после "+7"')
-        
-        else:
-            return f'+7{telephone_number}'
+        return f'+7{int(telephone_number)}'
